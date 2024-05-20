f62a89c46dfaa90930cd2bc6c0004f5cb5aa2737
Leire Anne Retegui Schiettekatte <leiretsch@gmail.com>
2024-05-20 11:44:04 +0200

    Modified figure representation to show station as subtiles.

--- C02_MAIN_compare_ERA5_GNSS_PWV.m
+++ C02_MAIN_compare_ERA5_GNSS_PWV.m
@@ -16,13 +16,15 @@ load([input_fn_GNSS.dir '/' input_fn_GNSS.PWV])
 
 ia = ismember(time_ERA,time);
 
+figure; t = tiledlayout('flow','TileSpacing','tight');
 for i=1:size(PWV,2) % For each station
-    figure;
+    nexttile;
     plot(time,PWV(:,i)); hold on;
     plot(time_ERA(ia),PWV_ERA(ia,i));
-    legend({'GNSS','ERA5'})
-    title(sta_list_geod(i,:))
+    title(sta_list_geod(i,:)); ylabel('PWV (mm)');
 end
+
+legend({'GNSS','ERA5'});
 
 bias = mean(PWV - PWV_ERA(ia,:),1,'omitnan')';
 std_ = std(PWV - PWV_ERA(ia,:),[],1,'omitnan')';
